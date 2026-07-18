ae432f989495781a848324461d96b9fc4f243a19
cagri-1453 <cagri198566@gmail.com>
2026-07-19 00:36:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,1 @@
-import os
-# Değişkenleri her zaman Render'ın güvenli Environment sekmesinden al
-API_KEY = os.environ.get("cn6zvjYROGLnKFOYgYWQo0GF4")
-API_SECRET = os.environ.get("EryNYsgIu4P9Gl9RAWC04cB9L6cFbbI2yEqa9HND0qPP6rVJbb")
-ACCESS_TOKEN = os.environ.get("457483523-hsBomhqHfpdqeWlJYiFOmBIjTjgOgvW8pN9FFevk")
-ACCESS_TOKEN_SECRET = os.environ.get("xlc2xEb7HfmuyCnDkjUyhiREsCF0uNqXHFmfKjMN40nt0")
+
